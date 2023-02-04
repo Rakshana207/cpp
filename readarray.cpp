62d2812da7af0892f7548f695768b80553a28b10
Rakshana207 <112973484+Rakshana207@users.noreply.github.com>
2023-02-04 22:22:34 +0530

    Add files via upload

---
+++ readarray.cpp
@@ -0,0 +1,19 @@
+//AUTHOR:K.Rakshana DATE:31-1-2023
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,numbers[10];
+    cout<<"Enter the integers\n";
+    for(i=0;i<10;i++)
+    {
+        cin>> numbers[i];
+    }
+    cout<<"the integers are: \n";
+    for(i=0;i<10;i++)
+    {
+        cout<< numbers[i]<< "\n";
+    }
+    return 0;
+}

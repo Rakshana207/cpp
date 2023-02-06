53acc7294c166d85846242f614362a3cb1097260
Rakshana207 <112973484+Rakshana207@users.noreply.github.com>
2023-02-06 23:19:23 +0530

    Add files via upload

---
+++ sec_smalllarge.cpp
@@ -0,0 +1,25 @@
+//AUTHOR:K.Rakshana DATE:5-02-2023
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the size of the array:";
+    cin>>n;
+    
+    int arr[n];
+    cout<<"Enter the elements \n";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    sort(arr,arr+n);
+    cout<<"\nelements in the array:";
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+    cout<<"\nThe second smallest number is "<<arr[1]<<"\n";
+    cout<<"The second highest number is "<<arr[n-2];
+}
